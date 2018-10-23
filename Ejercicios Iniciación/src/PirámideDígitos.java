d8ad85d5622564f72b9887ba5fa7c2079b6bbdcf
Julio P. López Bayón <jplbayon@gmail.com>
2018-10-23 10:20:05 +0200

    Pirámide de dígitos sin terminar.

---
+++ Ejercicios Iniciación/src/PirámideDígitos.java
@@ -0,0 +1,21 @@
+
+public class PirámideDígitos {
+
+	public static void main(String[] args) {
+		for (int i=1; i<=10; i++) {
+			int n = i % 10;
+			int espacios = 10 - i;
+			int digitos = i + i - 1;
+			
+			for (int j = 0; j < espacios; j++)
+				System.out.print(" ");
+			
+			for () {
+				
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
