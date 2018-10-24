c0471d4e34f9c7248998b3e2a159d5bc8d4d31db
Julio P. López Bayón <jplbayon@gmail.com>
2018-10-24 13:08:07 +0200

    Pirámide de dígitos terminado.

--- Ejercicios Iniciación/src/PirámideDígitos.java
+++ Ejercicios Iniciación/src/PirámideDígitos.java
@@ -2,18 +2,21 @@
 public class PirámideDígitos {
 
 	public static void main(String[] args) {
-		for (int i=1; i<=10; i++) {
+		for (int i = 1; i <= 10; i++) {
 			int n = i % 10;
 			int espacios = 10 - i;
 			int digitos = i + i - 1;
-			
+
 			for (int j = 0; j < espacios; j++)
 				System.out.print(" ");
-			
-			for () {
-				
+
+			for (int h = 0; h < digitos; h++) {
+				if (h >= digitos / 2) {
+					System.out.print(n--%10);
+				} else if (h <= digitos / 2) {
+					System.out.print(n++%10);
+				}
 			}
-			
 			System.out.println();
 		}
 	}
